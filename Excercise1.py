c71c32ffa8956017c7ead710e2172a5b5bbe8a9c
wszolki <78880390+wszolki@users.noreply.github.com>
2025-05-25 10:08:42 +0200

    Adding the file Exercise1.py

---
+++ Excercise1.py
@@ -0,0 +1,8 @@
+# Write a program that checks whether a number entered by the user is even or odd.
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
